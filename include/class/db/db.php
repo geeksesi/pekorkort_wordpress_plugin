d0afde504c614112d7a855d389aaae340092e7c6
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-09-21 11:46:48 +0430

    database class defined for global use

--- include/class/db/db.php
+++ include/class/db/db.php
@@ -11,16 +11,19 @@ require_once(__DIR__ . "/Categories.php");
 global $jal_db_version;
 $jal_db_version = '0.1.0';
 
+$quest         = new Quest($wpdb);
+$exam          = new Exam($wpdb);
+$categories    = new Categories($wpdb);
 
 function jal_install()
 {
    error_log("im here");
    global $wpdb;
+   global $quest;
+   global $exam;
+   global $categories;
 
-   $quest         = new Quest($wpdb);
-   $exam          = new Exam($wpdb);
-   $categories    = new Categories($wpdb);
-
+   
    $table_prefix     = $wpdb->prefix;
    $charset_collate  = $wpdb->get_charset_collate();
 
