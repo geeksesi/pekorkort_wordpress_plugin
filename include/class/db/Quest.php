f8b308ecb45b7b4f94dfd3f4c6093d2ddf107fd6
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-09-21 10:59:15 +0430

    quest migration

--- include/class/db/Quest.php
+++ include/class/db/Quest.php
@@ -1,14 +1,22 @@
 <?php
+defined('ABSPATH') || exit;
+
 
 class Quest
 {
     function __construct()
     { }
 
-
-    function init($_prefix, $_charset)
+    /**
+     * retun sql query for migration
+     *
+     * @param   String  $_prefix
+     * @param   String  $_charset
+     * @return  String  $sql
+     */
+    function migration($_prefix, $_charset)
     {
-        $table_name = $_prefix . "_pekorkort_quest";
+        $table_name = $_prefix . "pekorkort_quest";
         $sql = "CREATE TABLE $table_name (
             id              int(9)          NOT NULL AUTO_INCREMENT,
             quest           text            NOT NULL,
@@ -18,10 +26,12 @@ class Quest
             season_id       int(10)         NOT NULL,
             level           int(2)          NOT NULL,
             wrong_refer     varchar(55)     NOT NULL,
-            correct_refer  varchar(55)     NOT NULL,
-            timestamp       bigint(55)      NOT NULL,
+            correct_refer   varchar(55)     NOT NULL,
+            timestamp       bigint(12)      NOT NULL,
             
             PRIMARY KEY  (id)
           ) $_charset;";
+
+        return $sql;
     }
 }
