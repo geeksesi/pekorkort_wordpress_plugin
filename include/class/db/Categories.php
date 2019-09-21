651b805e5801a611bf3e0eb9c943d3a68d104665
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-09-21 14:06:05 +0430

    Category db

--- include/class/db/Categories.php
+++ include/class/db/Categories.php
@@ -7,28 +7,87 @@ defined('ABSPATH') || exit;
 class Categories
 {
     private $wpdb;
-    function __construct($_wpdb)
-    { 
+    private $table_name;
+    public function __construct($_wpdb, $_prefix)
+    {
         $this->wpdb = $_wpdb;
+        $this->table_name = $_prefix . "pekorkort_categories";
     }
-
 
     /**
      * retun sql query for migration
      *
-     * @param   String  $_prefix
-     * @param   String  $_charset
+     * @param   [type]  $_charset
      * @return  String  $sql
      */
-    function migration($_prefix, $_charset)
+    public function migration($_charset)
     {
-        $table_name = $_prefix . "pekorkort_categories";
-        $sql = "CREATE TABLE $table_name (
+        $sql = "CREATE TABLE $this->table_name (
             id       int(9)         NOT NULL AUTO_INCREMENT,
-            name     varchar(30     NOT NULL,            
+            name     varchar(30)    NOT NULL,            
             PRIMARY KEY  (id)
           ) $_charset;";
 
         return $sql;
+    }
+
+    /**
+     * set data
+     *
+     * @param string $_name
+     * @return boolean
+     */
+    public function set_data(string $_name)
+    {
+        if (empty($_name))
+            return false;
+
+        $query_result = $this->wpdb->insert($this->table_name, ["name" => $_name], "%s");
+        return $query_result;
+    }
+
+
+    /**
+     * get by ID
+     *
+     * @param integer $_id
+     * @return boolean | array
+     */
+    public function get_id(int $_id)
+    {
+        if (!is_numeric($_id))
+            return false;
+
+        $category = $this->wpdb->get_row("SELECT * FROM " . $this->table_name . " WHERE id = " . $_id, ARRAY_A);
+        return $category;
+    }
+
+    /**
+     * get all categories
+     *
+     * @return Array
+     */
+    public function get_all()
+    {
+        $categories = $this->wpdb->get_results("SELECT * FROM " . $this->table_name);
+        return $categories;
+    }
+
+
+    /**
+     * update name of category
+     *
+     * @param integer $_id
+     * @param string $_new_name
+     * @return boolean
+     */
+    public function update_name(int $_id, string $_new_name)
+    {
+        if (!is_numeric($_id) ||  !is_string($_new_name))
+            return false;
+
+        $sql_result = $this->wpdb->update($this->table_name, ["name" => $_new_name], ["id" => $_id], "%s", "%d");
+
+        return $sql_result;
     }
 }
