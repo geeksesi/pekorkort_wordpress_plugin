fc7c2b4463d4298aa8c1ebcae71e7378c74af56c
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-09-22 14:56:10 +0330

    v0.2.0 in way

--- PekorKort.php
+++ PekorKort.php
@@ -11,26 +11,27 @@ Text Domain: -e('PekorKort-wp','PekorKort-wp')
 defined('ABSPATH') || exit('No Direct Access.');
 define('PEKORKORT_DIR', plugin_dir_path(__FILE__));
 define('PEKORKORT_URL', plugin_dir_url(__FILE__));
-define('PEKORKORT_CSS_URL', trailingslashit(PEKORKORT_URL.'assets/css'));
-define('PEKORKORT_JS_URL', trailingslashit(PEKORKORT_URL.'assets/js'));
-define('PEKORKORT_IMG_URL', trailingslashit(PEKORKORT_URL.'assets/img'));
-define('PEKORKORT_INC_DIR', trailingslashit(PEKORKORT_DIR.'include'));
-define('PEKORKORT_ADMIN_DIR', trailingslashit(PEKORKORT_DIR.'admin'));
-define('PEKORKORT_TPL_DIR', trailingslashit(PEKORKORT_DIR.'template'));
+define('PEKORKORT_CSS_URL', trailingslashit(PEKORKORT_URL . 'assets/css'));
+define('PEKORKORT_JS_URL', trailingslashit(PEKORKORT_URL . 'assets/js'));
+define('PEKORKORT_IMG_URL', trailingslashit(PEKORKORT_URL . 'assets/img'));
+define('PEKORKORT_INC_DIR', trailingslashit(PEKORKORT_DIR . 'include'));
+define('PEKORKORT_ADMIN_DIR', trailingslashit(PEKORKORT_DIR . 'admin'));
+define('PEKORKORT_TPL_DIR', trailingslashit(PEKORKORT_DIR . 'template'));
 define('PEKORKORT_VERSION', "0.1.0");
 
-require PEKORKORT_INC_DIR.'class/db/db.php';
-require PEKORKORT_INC_DIR.'assets.php';
-require PEKORKORT_INC_DIR.'shortcodes.php';
-
+include PEKORKORT_INC_DIR . 'class/db/db.php';
+include PEKORKORT_INC_DIR . 'class/Generator/Generator.php';
+require PEKORKORT_INC_DIR . 'assets.php';
+require PEKORKORT_INC_DIR . 'shortcodes.php';
+// require PEKORKORT_DIR . 'sample/dbSample.php';
 
 register_activation_hook(__FILE__, 'jal_install');
 add_action('plugins_loaded', 'PEKORKORT_textdomain');
-function PEKORKORT_textdomain() {
-	load_plugin_textdomain( 'PekorKort-wp', false, dirname( plugin_basename(__FILE__) ) . '/languages/' );
-}
-if(is_admin())
+function PEKORKORT_textdomain()
 {
-	require_once PEKORKORT_ADMIN_DIR.'admin.php';
+	load_plugin_textdomain('PekorKort-wp', false, dirname(plugin_basename(__FILE__)) . '/languages/');
 }
-// require PEKORKORT_INC_DIR."class/Bitcoin.php";
+
+if (is_admin()) {
+	require_once PEKORKORT_ADMIN_DIR . 'admin.php';
+}
