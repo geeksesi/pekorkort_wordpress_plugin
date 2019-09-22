e94af39c4359e6b6045159aa326595068a6129c6
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-09-22 14:55:32 +0330

    test Exam Generator

--- template/html-admin-main.php
+++ template/html-admin-main.php
@@ -1,5 +1,23 @@
 <div id="PekorKort">
 
-<h1>Hello World</h1>
+    <h1>Hello World</h1>
+
+    <br>
+    <br>
+    <br>
+
+    <?php
+
+    var_dump($exam_generator->new_exam(0, [
+        "new_only"      => false,
+        "emptys"        => true,
+        "wrongs"        => true,
+        "answer_result" => false,
+        "random"        => true,
+        "length"        => 2,
+        "category"      => '',
+    ]));
+
+    ?>
 
 </div>
