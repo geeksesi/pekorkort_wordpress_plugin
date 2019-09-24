b6232b91155d725e118c2bff018b2b4dfeda5962
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-09-24 13:48:52 +0330

    checks finished about finish exam

--- include/class/Api/ExamRestApi.php
+++ include/class/Api/ExamRestApi.php
@@ -16,7 +16,7 @@ class ExamRestApi extends ApiBase
 
 
     /**
-     * must check this keys from $_POST
+     * must check and return this keys from $_POST
      *  token         [string]
      *  new_only      [boolean]
      *  emptys        [boolean]
@@ -46,14 +46,26 @@ class ExamRestApi extends ApiBase
             (bool)   "answer_result" => $_POST["answer_result"],
             (bool)   "random"        => $_POST["random"],
             (int)    "length"        => $_POST["length"],
-            (string) "category"      => $_POST["category"],
+            (array)  "category"      => json_decode($_POST["category"]),
         ];
         return $output;
     }
 
 
-    public function check_access_to_option(array $_option, int $_access_code)
+    /**
+     * check this option with user access
+     *
+     * @param array $_option
+     * @param integer $_user_id
+     * @return boolean
+     */
+    public function check_access_to_option(array $_option, int $_user_id)
     {
+        $user_access_code = $this->check_user_access((int) $_user_id);
+        if (!$user_access_code) {
+            return false;
+        }
+
         return true;
     }
 
@@ -79,23 +91,88 @@ class ExamRestApi extends ApiBase
             return json_encode($output);
         }
 
-        $user_access_code = $this->check_user_access((int) $user_id);
-
-        if (!$user_access_code)
-        {
+        if (!$this->check_access_to_option($inputs, (int) $user_id)) {
             $output["ok"]      = false;
-            $output["message"] = "you has not access to this option";
+            $output["message"] = "access denied";
             return json_encode($output);
         }
-
 
         $output["ok"]      = true;
         $output["message"] = "ok";
         return json_encode($output);
     }
 
+
+    /**
+     * check and return this inputs from $_POST
+     *  token           [string]
+     *  exma_id         [int]
+     *  corrects        [JSON/array]
+     *  wrongs          [JSON/array]
+     *  emptys          [JSON/array]
+     *
+     * @return boolean|array
+     */
+    public function check_finish_inputs()
+    {
+        $output = [];
+        if (!isset($_POST["token"]) || !isset($_POST["exam_id"]) || !isset($_POST["corrects"]) || !isset($_POST["wrongs"]) || !isset($_POST["emptys"]))
+            return false;
+
+        if (!empty($_POST["token"]) || !empty($_POST["exam_id"]) || !empty($_POST["corrects"]) || !empty($_POST["wrongs"]) || !empty($_POST["emptys"]))
+            return false;
+
+        $output = [
+            (string) "token"    => $_POST["token"],
+            (int)    "exma_id"  => $_POST["exma_id"],
+            (array)  "corrects" => json_decode($_POST["corrects"]),
+            (array)  "wrongs"   => json_decode($_POST["wrongs"]),
+            (array)  "emptys"   => json_decode($_POST["emptys"]),
+        ];
+
+        return $output;
+    }
+
+
+    /**
+     * must check this user has this exam owner or not
+     *
+     * @param integer $_user_id
+     * @param integer $_exam_id
+     * @return boolean
+     */
+    public function check_exam_and_user(int $_user_id, int $_exam_id)
+    {
+        return true;
+    }
+
+
+    /**
+     * method to handle POST request by this url : `http://localhost/wordpress/wp-json/pekorkort-exam-api/v1/finish_exam`
+     * a port to finish exam data 
+     *
+     * @return string  json endode string
+     */
     public function finish_exam()
     {
-        return "hello world";
+        $inputs = $this->check_finish_inputs();
+        if (!is_array($inputs) || !$inputs) {
+            $output["ok"]      = false;
+            $output["message"] = "inputs faild";
+            return json_encode($output);
+        }
+
+        $user_id = $this->un_token($_POST["token"]);
+        if (!$user_id) {
+            $output["ok"]      = false;
+            $output["message"] = "User undefined";
+            return json_encode($output);
+        }
+
+        if (!$this->check_exam_and_user((int) $user_id, $inputs["exma_id"])) {
+            $output["ok"]      = false;
+            $output["message"] = "access denied";
+            return json_encode($output);
+        }
     }
 }
